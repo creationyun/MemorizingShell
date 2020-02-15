6e838b56fd4db9a7e45aa69ad36da136a7f9acd2
Creation Yun <creationyun@gmail.com>
2020-02-15 18:03:44 +0900

    Modify to quiz form

--- word.sh
+++ word.sh
@@ -3,32 +3,49 @@
 clear
 
 if [ -z $1 ]; then
-	echo "Argument: ./word.sh filename.csv"
+	echo "Usage: word.sh [filename.csv]"
 	exit
 fi
 
 # save previous IFS
 OLDIFS=$IFS
-# new IFS: ,
+# new IFS: comma
 IFS=","
 
-# word number
-num=1
+# initial variable
+num=0
+wordlist=()
+wordmean=()
 
 # read csv and line (3 columns)
 while read f1 f2
 do
-    echo "No. $num"
-    echo
-    echo "     $f1"  # English word
-    echo
-    sleep 3
-    echo "     $f2"  # Meaning
-    echo
-    sleep 1
-    clear
+    wordlist[$num]=$f1
+    wordmean[$num]=$f2
     num=`expr $num + 1`
 done < $1
+
+# start quiz
+for (( i = 0 ; i < ${#wordlist[@]} ; i++ )); do
+    echo "No. $((i+1))"
+    echo
+    echo "     ${wordlist[$i]}"  # English word
+    echo
+    read -p "? " userinput
+    echo
+    echo "     ${wordmean[$i]}"  # Meaning
+    echo
+    
+    if [ x${wordmean[$i]} == x$userinput ]
+    then
+        echo "Correct!"
+    else
+        echo "Wrong..."
+    fi
+
+    read -p "Press ENTER to go next..."
+    clear
+done
 
 # restore IFS
 IFS=$OLDIFS
